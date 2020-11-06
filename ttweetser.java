bca85caeceba472ebe10fa388f0f40f78ac2821b
Molly Williams <mollywilliams@Mollys-MacBook-Pro-3.local>
2020-11-05 19:43:58 -0500

    broke it lol

--- ttweetser.java
+++ ttweetser.java
@@ -21,7 +21,14 @@ public class ttweetser {
 
                 //in and out streams
                 DataInputStream in = new DataInputStream(socket.getInputStream());
+                //InputStream in = socket.getInputStream();
                 DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+                //BufferedReader d = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                //BufferedReader reader = new BufferedReader(new InputStreamReader(in)); //pull information from socket
+                //String line = reader.readLine();
+                System.out.println(in);
+                //System.out.println(d.readLine());
+
 
                 Thread newThread = new ClientHandler(socket, in, out);
 
@@ -57,15 +64,30 @@ class ClientHandler extends Thread {
                 out.writeUTF("Whats up");
                 received = in.readUTF();
                 System.out.println("check Server");
-
-                if(received.equals("exit"))
-                {
+                if (received.length() > 7 && received.substring(0,7).equals("tweet \"")) {
+                    String theTweet = received.substring(7, received.length());
+                    //tweet logic
+                } else if (received.length() > 13 && received.substring(0,13).equals("unsubscribe #")) {
+                    //unsubscribe logic
+                } else if (received.length() > 11 && received.substring(0,11).equals("subscribe #")) {
+                    //subscribe logic
+                } else if (received.equals("timeline")) {
+                    //timeline logic
+                } else if (received.equals("exit")) {
                     System.out.println("Client " + this.socket + " sends exit...");
                     System.out.println("Closing this connection.");
                     this.socket.close();
                     System.out.println("Connection closed");
+                    //add logic to have user info removed
                     break;
+                } else if (received.equals("getusers")) {
+                    //getusers logic
+                } else if (received.equals("gettweets")) {
+                    //gettweets logic
+                } else {
+                    //invalid request logic
                 }
+
                 break;
 
             } catch (IOException e) {
